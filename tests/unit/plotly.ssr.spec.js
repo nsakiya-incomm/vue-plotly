0d511392acc86dc3f02baadc5a736af437da0865
Natthavut Sakiya <nsakiya@incomm.com>
2022-06-27 18:00:08 -0700

    ran prepublishedbuild

--- tests/unit/plotly.ssr.spec.js
+++ tests/unit/plotly.ssr.spec.js
@@ -5,7 +5,9 @@
 const Vue = require("vue");
 const renderer = require("vue-server-renderer").createRenderer();
 const Plotly = require("@/components/Plotly").default;
+  /* eslint-disable */
 Vue.component("plotly", Plotly);
+  /* eslint-enable */
 const app = new Vue({
   name: "test-app",
   template: `<plotly></plotly>`
